737e65f7ae04bc12e4b9a6bd4c0386be6f0051be
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-10-29 15:40:22 +0530

    Update flight_delay.py

--- flight_delay.py
+++ flight_delay.py
@@ -44,7 +44,7 @@ X = preprocessing.scale(X)
 
 X_train,X_test,y_train,y_test = model_selection.train_test_split(X,y,test_size = 0.2)
 
-clf = linear_model.LogisticRegression()
+clf = SVC(kernel="linear",gamma='auto',C=10)
 clf.fit(X_train,y_train)
 
 print(clf.score(X_test,y_test))
